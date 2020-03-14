ef0e08564eaaf97d9d2efe326f2a8b92ce8f6ed5
Carlos Frias <carlos.a.frias@gmail.com>
2020-03-14 12:36:02 -0300

    Connect packages

---
+++ packages/engine/src/models/index.ts
@@ -0,0 +1,9 @@
+export { default as Address } from "./Address";
+export { default as Area } from "./Area";
+export { default as City } from "./City";
+export { default as Money } from "./Money";
+export { default as Offer } from "./Offer";
+export { default as Property } from "./Property";
+export { default as RentOffer } from "./RentOffer";
+export { default as SellingOffer } from "./SellingOffer";
+export { default as User } from "./User";
